3b984e1487a32361105c8fed763d53ecbcc21b6d
Natalia Bogdanova <tasha1997@mail.ru>
2017-09-26 22:33:39 +0300

    modified:   01/tasks.js

--- 01/tasks.js
+++ 01/tasks.js
@@ -5,7 +5,20 @@
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
 function getMinMax(string) {
-
+  if (!string.length) {
+    return null;
+  }
+  let arrStr = string.split(/[ ,:;!?`"&|()<>{}\[\]\r\n/\\~]+/);
+  let minNum = 1/0;
+  let maxNum = -1/0;
+  for (let i = 0; i < arrStr.length; ++i) {
+    let curNum = parseFloat(arrStr[i]);
+    if (curNum) {
+      minNum = Math.min(minNum, curNum);
+      maxNum = Math.max(maxNum, curNum);
+    }
+  }
+  return {min: minNum, max: maxNum};
 }
 
 /* ============================================= */
@@ -16,7 +29,9 @@ function getMinMax(string) {
  * @return {number} число под номером х
  */
 function fibonacciSimple(x) {
-  return x;
+  if (x === 0) return 0;
+  if (x === 1) return 1;
+  return fibonacciSimple(x - 1) + fibonacciSimple(x - 2);
 }
 
 /* ============================================= */
@@ -28,7 +43,15 @@ function fibonacciSimple(x) {
  * @return {number} число под номером х
  */
 function fibonacciWithCache(x) {
-  return x;
+  let myX = x;
+  let firstFib = 0;
+  let secondFib = 1;
+  while(myX) {
+    secondFib += firstFib;
+    firstFib = secondFib - firstFib;
+    --myX;
+  }
+  return firstFib;
 }
 
 /* ============================================= */
@@ -49,19 +72,50 @@ function fibonacciWithCache(x) {
  * @return {string}
  */
 function printNumbers(max, cols) {
-
+  let ansStr = '';
+  let rows = Math.ceil(max / cols);
+  for (let curRow = 0; curRow < rows; ++curRow) {
+    for (let curCol = 0; curCol < cols; ++curCol) {
+      let curNum = curCol * rows + curRow;
+      if (!curNum / 10) {
+        ansStr += ' ';
+      } 
+      ansStr += curNum;
+    }
+    ansStr += '\n';
+  }
 }
 
 /* ============================================= */
 
 /**
  * Реализуйте RLE-сжатие: AAAB -> A3B, BCCDDDEEEE -> BC2D3E4
- * @param  {string} value
+ * @param  {string} input
  * @return {string}
  */
 function rle(input) {
-
+  if (!input.length) {
+    return '';
+  }
+  let curChar = input[0];
+  let ansStr = '' + input[0];
+  let counter = 1;
+  for (let i = ; i < input.length; ++i) {
+    if (curChar === input[i]) {
+      ++counter;
+    } else {
+      if (counter != 1) {
+        ansStr += counter;
+      }
+      counter = 0;
+      ansStr += input[i];
+      curChar = input[i];
+    }
+  }
+  return ansStr;
 }
+
+
 
 module.exports = {
   getMinMax,
