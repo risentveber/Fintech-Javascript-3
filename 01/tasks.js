651b13a979e0bdfbd5b80e28ef1843bc8b16affd
elinrin <elin.rinnel@gmail.com>
2017-09-28 20:37:51 +0300

    lesson-01

--- 01/tasks.js
+++ 01/tasks.js
@@ -5,7 +5,27 @@
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
 function getMinMax(string) {
+  const array = string.split(/[\s,;:]+/);
+  const objectMinMax = { min: null, max: null };
 
+  for (let i = 0; i < array.length; i++) {
+    const number = +array[i];
+
+    if (!isNaN(number)) {
+      if (objectMinMax.min === null) {
+        objectMinMax.min = number;
+      } else if (objectMinMax.min > number) {
+        objectMinMax.min = number;
+      }
+      if (objectMinMax.max === null) {
+        objectMinMax.max = number;
+      } else if (objectMinMax.max < number) {
+        objectMinMax.max = number;
+      }
+    }
+  }
+
+  return objectMinMax;
 }
 
 /* ============================================= */
@@ -16,7 +36,7 @@ function getMinMax(string) {
  * @return {number} число под номером х
  */
 function fibonacciSimple(x) {
-  return x;
+  return (x === 0 || x === 1) ? x : fibonacciSimple(x - 1) + fibonacciSimple(x - 2);
 }
 
 /* ============================================= */
@@ -27,9 +47,23 @@ function fibonacciSimple(x) {
  * @param {number} x номер числа
  * @return {number} число под номером х
  */
-function fibonacciWithCache(x) {
-  return x;
-}
+const fibonacciWithCache = (function() {
+  const memo = {};
+
+  function fibonacci(x) {
+    let value;
+
+    if (x in memo) {
+      value = memo[x];
+    } else {
+      value = (x === 0 || x === 1) ? x : fibonacci(x - 1) + fibonacci(x - 2);
+      memo[x] = value;
+    }
+    return value;
+  }
+
+  return fibonacci;
+}());
 
 /* ============================================= */
 
@@ -49,7 +83,20 @@ function fibonacciWithCache(x) {
  * @return {string}
  */
 function printNumbers(max, cols) {
+  let string = '';
+  const row = Math.ceil(max / cols);
 
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (i + j * row <= max) {
+        string += (i + j * row < 10) ? ` ${i + j * row} ` : `${i + j * row} `;
+      } else {
+        break;
+      }
+    }
+    string = `${string.slice(0, -1)}\n`;
+  }
+  return string.slice(0, -1);
 }
 
 /* ============================================= */
@@ -60,8 +107,27 @@ function printNumbers(max, cols) {
  * @return {string}
  */
 function rle(input) {
+  const newString = [],
+    array = input.split('');
+  let newPosition = 0,
+    symbolRepeat = 0,
+    currUniqueSymbol = array[0];
 
+  for (let j = 0; j < array.length; j++) {
+    if (currUniqueSymbol !== array[j]) {
+      newString[newPosition++] = currUniqueSymbol + ((symbolRepeat < 2) ? '' : symbolRepeat);
+      currUniqueSymbol = array[j];
+      symbolRepeat = 1;
+    } else {
+      symbolRepeat++;
+    }
+  }
+
+  newString[newPosition] = currUniqueSymbol + ((symbolRepeat < 2) ? '' : symbolRepeat);
+
+  return newString.join('');
 }
+
 
 module.exports = {
   getMinMax,
