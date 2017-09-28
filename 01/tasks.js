f8a1d942bc13256e17b207a0eda787d27b00eb50
rysja <rysja20061@yandex.ru>
2017-09-28 17:52:03 +0300

    lesson-01

--- 01/tasks.js
+++ 01/tasks.js
@@ -5,21 +5,17 @@
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
 function getMinMax(string) {
+  var str = string.match(/\-?\d*\.?\d+/g);
 
-      var string = string.match(/\-?\d*\.?\d+/g);
-      string.sort(function (a, b) {
-        return a - b;
-      });
+  str.sort((a, b) => a - b);
+  var objStr = {
+    max: str[str.length - 1],
+    min: str[0]
+  };
 
-      var objStr = {
-        max: string[string.length - 1],
-        min: string[0],
-      }
-      return objStr; 
+  return objStr;
 }
-
 /* ============================================= */
-
 /**
  * Напишите рекурсивную функцию вычисления чисел Фибоначчи
  * @param {number} x номер числа
@@ -28,9 +24,7 @@ function getMinMax(string) {
 function fibonacciSimple(x) {
   return x <= 1 ? x : fibonacciSimple(x - 1) + fibonacciSimple(x - 2);
 }
-
 /* ============================================= */
-
 /**
  * Напишите функцию для вычисления числа Фибоначчи с мемоизацией:
  * при повторных вызовах функция не вычисляет значения, а достает из кеша.
@@ -38,18 +32,20 @@ function fibonacciSimple(x) {
  * @return {number} число под номером х
  */
 function fibonacciWithCache(x) {
-      var memo = [0, 1];
-      var fib = function (x) {
-          var result = memo[x];
-          if (typeof result !== 'number') {
-            result = fib(x - 1) + fib(x - 2);
-            memo[x] = result;
-          }
-          return result;
-      };
-      return fib(x);
-}
+  var memo = [0, 1];
 
+  var fib = x => {
+    var result = memo[x];
+
+    if (typeof result !== 'number') {
+      result = fib(x - 1) + fib(x - 2);
+      memo[x] = result;
+    }
+    return result;
+  };
+
+  return fib(x);
+}
 /* ============================================= */
 
 /**
@@ -69,38 +65,44 @@ function fibonacciWithCache(x) {
  */
 function printNumbers(max, cols) {
   var result = [];
-   var printNumberAtPosition = (number, row, column) => {
-    if (result[row] === undefined) {
-     result[row] = [];
-    }
-    number = number < 10 ? ' ' + number : '' + number;
-    result[row][column] = number;
-   };
-   var rows = 1;
-   while (rows * cols <= max) {
-    rows++;
-   }
-   var current = 0;
-   var column = 0;
-   while (current <= max) {
-    var row = 0;
-    while (row < rows && current <= max) {
-     printNumberAtPosition(current++, row, column);
-     row++;
-    }
-    column++;
-   }
-   var str = "";
-   for (row = 0; row < result.length; row++) {
-    var buffer = [];
-    for (column = 0; column < result[row].length; column++) {
-     buffer.push(result[row][column]);
-    }
-    str += buffer.join(' ') + '\n';
-   }
-   return str.substring(0, str.length - 1);
-}
 
+  var printNumberAtPosition = (number, row, column) => {
+    if (result[row] === undefined) {
+      result[row] = [];
+    }
+    number = number < 10 ? ` ${number}` : `${number}`;
+    result[row][column] = number;
+  };
+  var rows = 1;
+
+  while (rows * cols <= max) {
+    rows += 1;
+  }
+  var current = 0,
+    column = 0;
+
+  var row;
+
+  while (current <= max) {
+    row = 0;
+    while (row < rows && current <= max) {
+      printNumberAtPosition(current++, row, column);
+      row += 1;
+    }
+    column += 1;
+  }
+  var str = '';
+
+  for (row = 0; row < result.length; row++) {
+    var buffer = [];
+
+    for (column = 0; column < result[row].length; column++) {
+      buffer.push(result[row][column]);
+    }
+    str += `${buffer.join(' ')}\n`;
+  }
+  return str.substring(0, str.length - 1);
+}
 /* ============================================= */
 
 /**
@@ -110,17 +112,16 @@ function printNumbers(max, cols) {
  */
 function rle(input) {
   var n = '';
+
   input.match(/(.)\1*/g).forEach(a => {
-    if(a.length > 1){
+    if (a.length > 1) {
       n += a[0] + a.length;
+    } else {
+      n += a[0];
     }
-    else{
-     n += a[0] 
-    }         
-  })
+  });
   return n;
 }
-
 module.exports = {
   getMinMax,
   rle,
