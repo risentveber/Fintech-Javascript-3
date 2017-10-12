715c4c8e74808de412b087e0c738cf559d255e6d
Michael <Solonin37@mail.ru>
2017-10-12 23:16:53 +0300

    Add files via upload

--- 03/4/task.js
+++ 03/4/task.js
@@ -6,7 +6,11 @@
  */
 
 function promiseRace(promises) {
-  return Promise.resolve(null);
+  return new Promise(function (resolve, reject) {
+    promises.forEach(promise => {
+      promise.then(resolve, reject);
+    });
+  });
 }
 
 module.exports = promiseRace;
